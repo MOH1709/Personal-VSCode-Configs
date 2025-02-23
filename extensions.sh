590157b824c0d377eef47ea5e62c3b728361c49c
mohit <ahirwalmohit3216@gmail.com>
2025-02-23 19:28:38 +0530

    updated to quick install the extensions and add keyboard shortcuts

---
+++ extensions.sh
@@ -0,0 +1,15 @@
+code --install-extension esbenp.prettier-vscode --force &&
+code --install-extension formulahendry.auto-close-tag --force &&
+code --install-extension formulahendry.auto-complete-tag --force &&
+code --install-extension formulahendry.auto-rename-tag --force &&
+code --install-extension beardedbear.beardedtheme --force &&
+code --install-extension aaron-bond.better-comments --force &&
+code --install-extension streetsidesoftware.code-spell-checker --force &&
+code --install-extension usernamehw.errorlens --force &&
+code --install-extension dsznajder.es7-react-js-snippets --force &&
+code --install-extension github.copilot --force &&
+code --install-extension eamodio.gitlens --force &&
+code --install-extension wix.vscode-import-cost --force &&
+code --install-extension pkief.material-icon-theme --force &&
+code --install-extension esbenp.prettier-vscode --force &&
+code --install-extension ms-vscode-remote.remote-ssh --force
